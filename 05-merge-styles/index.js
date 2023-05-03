a0c2d09ef66ca7bb46fcbb9ae37039efd3facd1f
gtailde <gtailde@gmail.com>
2023-05-04 00:58:07 +0200

    fix: 05-merge-style tasks .js

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,36 +1,31 @@
 const fs = require('fs');
 const path = require("path");
-const bundleCSS = path.join(`05-merge-styles/project-dist/bundle.css`);
-const streamCreated = fs.createWriteStream(bundleCSS);
-read = (x) => {
-  const streamRead = new fs.ReadStream(path.join(__dirname, `styles/${x}`), 'UTF8');
-  let data = '';
-  streamRead.on('data', chunk => {data += chunk});
-  streamRead.on('end', function(){writeStyles(`${data}\n`)});
-  streamRead.on("error", err => {console.log('ERROR', err.message)});
-};
 
-writeStyles = (data) => {
-  streamCreated.write(data);
-};
+const stylesDir = path.join(__dirname, 'styles');
+const bundleCSS = path.join(__dirname, 'project-dist', 'bundle.css');
 
-fs.readdir(path.join(__dirname, './styles'), (err, files) => {
-  if(err) throw err; 
-  let filesCSS = [];
-  (files.join(' ').split(' ')).forEach((name) => {
-    filesCSS.push(name);
-  });
-  for(let i = 0; i < filesCSS.length; i++){
-    let x = filesCSS[i].split('.');
-    fs.stat(`05-merge-styles/styles/${filesCSS[i]}`, (err, stats) => {
-      if (err) {
-        console.error('ERROR', err.message);
-        return;
-      }
-      if(stats.isFile()){
-        if(x[x.length-1] === 'css' && stats.isFile()){read(filesCSS[i])};
-      }; 
-    });
+async function mergeStyles() {
+  try {
+    const files = await fs.promises.readdir(stylesDir);
     
-  };
-});
+    const cssFiles = files.filter(file => path.extname(file) === '.css');
+    
+    const data = await Promise.all(cssFiles.map(async file => {
+      const filePath = path.join(stylesDir, file);
+      return await fs.promises.readFile(filePath, 'utf8');
+    }));
+
+    const distDir = path.join(__dirname, 'project-dist');
+    if (!fs.existsSync(distDir)) {
+      fs.mkdirSync(distDir);
+    }
+
+    await fs.promises.writeFile(bundleCSS, data.join('\n'));
+    
+    console.log('Styles merged successfully');
+  } catch (err) {
+    console.error('Error:', err.message);
+  }
+}
+
+mergeStyles();
