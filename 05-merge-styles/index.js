820fa31212f3f911eefb4ed6fa86379018f517c6
gtailde <gtailde@gmail.com>
2023-05-10 22:52:04 +0200

    remove async method from 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,26 +1,32 @@
-const fs = require('fs');
-const path = require("path");
+const { promises: fsPromises } = require('fs');
+const path = require('path');
 
 const stylesDir = path.join(__dirname, 'styles');
 const bundleCSS = path.join(__dirname, 'project-dist', 'bundle.css');
 
 async function mergeStyles() {
   try {
-    const files = await fs.promises.readdir(stylesDir);
+    const files = await fsPromises.readdir(stylesDir);
     
     const cssFiles = files.filter(file => path.extname(file) === '.css');
     
     const data = await Promise.all(cssFiles.map(async file => {
       const filePath = path.join(stylesDir, file);
-      return await fs.promises.readFile(filePath, 'utf8');
+      return await fsPromises.readFile(filePath, 'utf8');
     }));
 
     const distDir = path.join(__dirname, 'project-dist');
-    if (!fs.existsSync(distDir)) {
-      fs.mkdirSync(distDir);
+    try {
+      await fsPromises.access(distDir);
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        await fsPromises.mkdir(distDir);
+      } else {
+        throw err;
+      }
     }
 
-    await fs.promises.writeFile(bundleCSS, data.join('\n'));
+    await fsPromises.writeFile(bundleCSS, data.join('\n'));
     
     console.log('Styles merged successfully');
   } catch (err) {
